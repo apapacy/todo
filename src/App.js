8d43be2b6ef989251123a7256fc28356baacd398
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2021-08-11 14:39:59 +0300

    add with props component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { TodoStateFull } from './components/TodoStateFull';
 import { TodoStateLess } from './components/TodoStateLess';
+import { TodoWithProps } from './components/TodoWithProps'
 
 function App() {
   return (
     <>
       <TodoStateFull/>
       <TodoStateLess/>
+      <TodoWithProps/>
     </>
   );
 }
---
+++ src/components/TodoWithProps.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+export const TodoWithProps = () => {
+  const [state, changeState] = useState({
+    title: 'To Do List',
+    todos: [],
+    value: ''
+  });
+
+  const handleChange = (event) => {
+    changeState({
+      ...state,
+      value: event.target.value
+    });
+  }
+
+  const handleClick = () => {
+    changeState({
+      ...state,
+      todos: state.todos.concat([state.value]),
+      value: '',
+    });
+  }
+  return (
+    <>
+      <Title title={state.title}/>
+      <TodoList todos={state.todos}/>
+      <Input handleChange={handleChange} value={state.value}/>
+      <Button handleClick={handleClick}/>
+    </>
+  );
+}
+
+const Title = ({ title }) => <h3>{ title }</h3>;
+const TodoList = ({ todos }) => <ul>
+    {
+      todos.map(todo => <li key={todo}>{todo}</li>)
+    }
+  </ul>;
+const Input = ({ value, handleChange }) => <input type="text" onChange={handleChange} value={value}/>;
+const Button = ({ handleClick }) => <button onClick={handleClick}>test</button>;
