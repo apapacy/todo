25a99f26ee9ff47ef0311fc5ef30d01941bba622
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2021-08-11 12:42:57 +0300

    statefull component

---
+++ src/components/TodoStateFull.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export class TodoStateFull extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'To Do List',
+      todos: [],
+      value: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value})
+  }
+
+  handleClick = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.concat([this.state.value]),
+      value: '',
+    });
+    this.state.todos.map(console.log)
+    console.log(this.state)
+  }
+
+  render = () => {
+    return (
+      <>
+        <h3>{this.state.title}</h3>
+        <ul>
+          {
+            this.state.todos.map(todo => <li key={todo}>{todo}</li>)
+          }
+        </ul>
+        <input type="text" onChange={this.handleChange} value={this.state.value}/>
+        <button onClick={this.handleClick}>test</button>
+      </>
+    );
+  }
+}
