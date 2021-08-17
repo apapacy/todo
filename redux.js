e4b7893e961661bbc4876f08ab173faf9f6e51e8
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2021-08-17 13:37:05 +0300

    initial value

--- redux.js
+++ redux.js
@@ -1,9 +1,14 @@
 const { createStore } = require('redux');
 
-function counterReducer(state = { value: 0 }, action) {
+const initialState = {
+  value: 0,
+  updatedAt: new Date(),
+};
+
+function counterReducer(state = initialState, action) {
   switch (action.type) {
     case 'counter/incremented':
-      return { value: state.value + 1, updatedAt: action.updatedAt };  
+      return { value: state.value + 1, updatedAt: action.updatedAt };
     case 'counter/decremented':
       return { value: state.value - 1, updatedAt: action.updatedAt };
     default:
@@ -15,8 +20,8 @@ const store = createStore(counterReducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+console.log(store.getState());
+
 store.dispatch({ type: 'counter/incremented', updatedAt: new Date() });
 store.dispatch({ type: 'counter/incremented', updatedAt: new Date() });
 store.dispatch({ type: 'counter/decremented', updatedAt: new Date() });
-
-
